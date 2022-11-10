21d1c368dc3c05bf1e7541c1afa2b3c069b80313
atikarahmanda <79711608+atikarahmanda@users.noreply.github.com>
2022-11-10 22:45:51 +0700

    Add files via upload

---
+++ wali.php
@@ -0,0 +1,38 @@
+<?php include'header.html' ?>
+  <div class="row">
+    <div class="col-sm">
+    <?php include 'navbardosen.php' ?>
+    </div>
+    <div class="col-10">
+        <div class="roq">
+        <div class="batas" style="text-align: center;font-size: 25px;font-weight: 200;">
+                    <h2>List Daftar Mahasiswa Wali <br>
+                    <?php echo $nama; ?> <br>
+                    Fakultas Sains dan Matematika UNDIP </h2>
+                 </div> 
+        </div>
+        <div class="">
+        <form action="" method="post">
+          <table class="table table-striped">
+            <tr>
+              <th>No</th>
+              <th>NIM</th>
+              <th>Nama</th>
+              <th>Angkatan</th>
+            </tr>
+
+            <?php
+            $no = 1;
+            $data = mysqli_query($db,"select * from mahasiswa
+            where email_dosenwali='$_SESSION[email]'");
+            while($d = mysqli_fetch_array($data)){
+                ?>
+                <tr>
+                  <td><?php echo $no++; ?></td>
+                  <td><?php echo $d['nim']; ?></td>
+                  <td><?php echo $d['nama']; ?></td>
+                  <td><?php echo $d['angkatan']; ?></td>
+                          </tr>
+                          <?php } ?>
+          </table>
+          </form>
