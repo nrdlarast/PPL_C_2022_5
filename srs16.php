32b19ad03e11f87f70624f8aefccf612f06c883d
VeronikaManalu <114333615+VeronikaManalu@users.noreply.github.com>
2022-11-11 07:40:12 +0700

    Add files via upload

---
+++ srs16.php
@@ -0,0 +1,55 @@
+<?php include'header.html' ?>
+  <div class="row">
+    <div class="col-sm">
+    <?php include 'navbardosen.php' ?>
+    </div>
+    <div class="col-10">
+        <div class="roq">
+        <div class="batas" style="text-align: center;font-size: 25px;font-weight: 200;">
+                    <h2>List Daftar dan Status Skripsi Mahasiswa Informatika  <br>
+                              Fakultas Sains dan Matematika UNDIP <br> </h2> <br>
+                 </div> 
+        </div>
+        <div class="">
+            <div class="row "style=" padding-block: 50px; padding-left: 50px; font-size: 20px;" >
+                    <div>
+                        <label for="pilihtahun">-pilih tahun-</label>
+                           <?php
+                            $now=date('Y');
+                            echo "<select name=’pilihtahun’>";
+                            for ($a=2016;$a<=$now;$a++)
+                            {
+                                echo "<option value='$a'>$a</option>";
+                            }
+                                echo "</select>";
+                           ?>
+                    </div>
+        </div>
+        <div class="navMenu">
+        <li class="active"><a href="srs16.php" style="color: black;">Lulus</a></li>
+        <li><a href="16belum.php" style="color: white;">Belum Lulus</a></li>
+                        </div>
+
+        <form action="" method="post">
+          <table class="table table-striped">
+            <tr>
+              <th>No</th>
+              <th>NIM</th>
+              <th>Nama</th>
+              <th>Angkatan</th>
+            </tr>
+
+            <?php
+            $no = 1;
+            $data = mysqli_query($db,"select * from skripsi join mahasiswa on mahasiswa.skripsi_id = skripsi.skripsi_id where status ='lulus'");
+            while($d = mysqli_fetch_array($data)){
+                ?>
+                <tr>
+                  <td><?php echo $no++; ?></td>
+                  <td><?php echo $d['nim']; ?></td>
+                  <td><?php echo $d['nama']; ?></td>
+                  <td><?php echo $d['angkatan']; ?></td>
+                          </tr>
+                          <?php } ?>
+          </table>
+          </form>
