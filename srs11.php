f492f2479a3abb3599c0fc0db6c03d9291d7b79b
Hana <89800565+hanajda@users.noreply.github.com>
2022-11-16 22:56:04 +0700

    Update srs11.php

--- srs11.php
+++ srs11.php
@@ -14,12 +14,12 @@
         </div>
             <div class="">
             <div class="row "style=" font-size: 20px;font-weight: 200; padding-left: 40px;" >
-                <div class="col" style="padding-left: 100px; padding-right: 100px;">
-					<div class="col" style="margin-top: 25px;margin-bottom: 10px;">
+                <div class="col" style="padding-left: 100px; padding-right: 100px; width: 33%; float: left">
+					<div class="col" style="margin-top: 25px;margin-bottom: 10px; text-align: center">
                         <label for="status">Mahasiswa Perwalian</label>
 					</div>
                     <div class="col"> <a href="wali.php">
-                        <div class="box" style="width: 200px; height: 200px; background-color: #EFEFEF;" >
+                        <div class="box" style="width: 100%; height: 200px; background-color: #EFEFEF;" >
                             <div style="margin-top: 25px;text-align:center; font-size: 70px;font-weight: 200;"> 
                                 <?php $data_mahasiswa = mysqli_query($db,"select * from mahasiswa join dosen on mahasiswa.email_dosenwali = dosen.email WHERE email_dosenwali='$_SESSION[email]'");
                                     $jumlah_mahasiswa = mysqli_num_rows($data_mahasiswa);
@@ -33,12 +33,12 @@
                     </div>
 
 				</div>
-                <div class="col" style="padding-left: 100px; padding-right: 100px;"> <a href="srs14.php">
-					<div class="col" style="margin-top: 25px;margin-bottom: 10px;">
+                <div class="col" style="padding-left: 100px; padding-right: 100px; width: 33%; float: left"> <a href="srs14.php">
+					<div class="col" style="margin-top: 25px;margin-bottom: 10px; text-align: center">
                         <label for="status">Mahasiswa PKL</label>
 					</div>
                     <div class="col">
-                        <div class="box" style="width: 200px; height: 200px; background-color: #EFEFEF;" >
+                        <div class="box" style="width: 100%; height: 200px; background-color: #EFEFEF;" >
                             <div style="margin-top: 25px;text-align:center; font-size: 75px;font-weight: 200;"> 
                                 <?php $data_mahasiswa = mysqli_query($db,"select * from mahasiswa join dosen on mahasiswa.email_dosenpkl = dosen.email WHERE email_dosenpkl='$_SESSION[email]'");
                                     $jumlah_mahasiswa = mysqli_num_rows($data_mahasiswa);
@@ -52,12 +52,12 @@
                     </div>
 
 				</div>
-                <div class="col" style="padding-left: 100px; padding-right: 100px;"> <a href="srs16.php">
-					<div class="col" style="margin-top: 25px;margin-bottom: 10px;">
+                <div class="col" style="padding-left: 100px; padding-right: 100px; width: 33%; float: left"> <a href="srs16.php">
+					<div class="col" style="margin-top: 25px;margin-bottom: 10px; text-align: center">
                         <label for="status">Mahasiswa Skripsi</label>
 					</div>
                     <div class="col" >
-                        <div class="box" style="width: 200px; height: 200px; background-color: #EFEFEF;" >
+                        <div class="box" style="width: 100%; height: 200px; background-color: #EFEFEF;" >
                             <div style="margin-top: 25px;text-align:center; font-size: 75px;font-weight: 200;"> 
                             <?php $data_mahasiswa = mysqli_query($db,"select * from mahasiswa join dosen on mahasiswa.email_dosenskripsi = dosen.email WHERE email_dosenskripsi='$_SESSION[email]'");
                                     $jumlah_mahasiswa = mysqli_num_rows($data_mahasiswa);
