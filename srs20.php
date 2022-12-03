d2536fd88ec5fff341be17dd83109f59b1e17a01
atikarahmanda <79711608+atikarahmanda@users.noreply.github.com>
2022-12-03 23:49:18 +0700

    Delete srs20.php

--- srs20.php
+++
@@ -1,8 +0,0 @@
-<?php include'header.html' ?>
-<?php error_reporting(0) ?>
-  <div class="row">
-    <div class="col-sm">
-    <?php include 'navbardepartemen.php' ?>
-    </div>
-    <?php include 'mahasiswa.php' ?>
-    
