d8b6089092b74990a28c7542e77cf738ba49afcb
atikarahmanda <79711608+atikarahmanda@users.noreply.github.com>
2022-12-03 23:49:54 +0700

    Delete srs24.php

--- srs24.php
+++
@@ -1,8 +0,0 @@
-<?php include'header.html' ?>
-<?php error_reporting(0) ?>
-  <div class="row">
-    <div class="col-sm">
-    <?php include 'navbardosen.php' ?>
-    </div>
-    <?php include 'mahasiswa.php' ?>
-    
