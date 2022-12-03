4d6c7918c26730741793da252769393bbbe4978e
atikarahmanda <79711608+atikarahmanda@users.noreply.github.com>
2022-12-03 23:49:38 +0700

    Delete srs22.php

--- srs22.php
+++
@@ -1,9 +0,0 @@
-<?php include'header.html' ?>
-<?php error_reporting(0) ?>
-  <div class="row">
-    <div class="col-sm">
-    <?php include 'navbaradmin.php' ?>
-    </div>
-    <?php include 'mahasiswa.php' ?>
-
-    
